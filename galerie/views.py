c72268b4452864162f69aefba9cddb3b7232aa47
Livia Degrossi <liviadegrossi@gmail.com>
2026-03-16 07:43:10 +0100

    Include dynamic data in the index.html

--- galerie/views.py
+++ galerie/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'galerie/index.html')
+    data = {
+        1: {'name': 'Nebulosa de Carina',
+            'legend': 'webbtelescope.org / NASA / James Webb'
+        },
+        2: {'name': 'Galáxia NGC 1079',
+            'legend': 'nasa.org / NASA / Hubble'
+        }
+    }
+    return render(request, 'galerie/index.html', {'cards': data})
 
 def imagem(request):
     return render(request, 'galerie/imagem.html')
