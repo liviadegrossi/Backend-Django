b57280016203e2f02b22a3f473864679dfc24423
Livia Degrossi <liviadegrossi@gmail.com>
2026-03-20 08:06:49 +0100

    Create the search functionaloty

--- galerie/urls.py
+++ galerie/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from galerie.views import index, imagem
+from galerie.views import index, imagem, search
 
 urlpatterns = [
     path('', index, name='index'),
-    path('imagem/<int:photo_id>', imagem, name='imagem')
+    path('imagem/<int:photo_id>', imagem, name='imagem'),
+    path('search', search, name='search')
 ]
--- galerie/views.py
+++ galerie/views.py
@@ -9,3 +9,16 @@ def index(request):
 def imagem(request, photo_id):
     photo = get_object_or_404(Photograph, pk=photo_id)
     return render(request, 'galerie/imagem.html', {"photo": photo})
+
+def search(request):
+    photos = Photograph.objects.order_by('date').filter(published=True)
+
+    # check whether the search term is in the request
+    if 'search' in request.GET: 
+        # get the search term in the request URL
+        name_to_search = request.GET['search'] # search is the name used in the input tag of the menu
+        # if there is a term to search, filter the photos by it
+        if name_to_search: 
+            photos = photos.filter(name__icontains=name_to_search)
+
+    return render(request, 'galerie/search.html', {'cards': photos})
